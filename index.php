7ff0cc0f54022a00efefa4418d4801fbe2bdf34e
Auguste-gmb <auguste.gambu@viacesi.fr>
2026-04-01 15:07:06 +0200

    fix: db acces

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     error_reporting(E_ALL);
 
     require_once __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
     use App\Core\Router;
 
--- src/Core/Database.php
+++ src/Core/Database.php
@@ -10,21 +10,25 @@ class Database
 {
     private static ?Database $instance = null;
     private PDO $pdo;
-    private $user = 'neipe';
-    private $pass = 'Neipe@2024!';
+    
 
     private function __construct()
     {
-        $dsn = "mysql:host=localhost;dbname=stagehub";
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_NAME'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASS'];
+
+        $dsn = "mysql:host=$host;dbname=$dbname";
 
         try {
-            $this->pdo = new PDO($dsn, $this->user, $this->pass, [
+            $this->pdo = new PDO($dsn, $user, $pass, [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
             ]);
         } catch (PDOException $e) {
-            die('Erreur BDD : ' . $e->getMessage()); // temporaire debug
+            die('Erreur BDD : ' . $e->getMessage()); // Debug tempo !!!!
         }
     }
 
